4db9f31368a1cf057f7f677782e698336c95d5e4
milindkusahu <milindsahu2000@gmail.com>
2025-03-29 04:44:51 +0530

    implement MongoDB connection logic and refactor server initialization

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,8 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
 
-import express from "express";
-const app = express();
+dotenv.config({
+  path: "./.env",
+});
 
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", () => {
-      console.log("ERR: ", error);
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR", error);
-  }
-})();
+connectDB();
